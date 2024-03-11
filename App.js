dcdbd14e37ff706ad2929c9d20dc960a2a322bff
Reaper2203 <134066384+Reaper2203@users.noreply.github.com>
2024-03-11 12:11:21 +0800

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 const HomeScreen = () => {
-  return {
+  return (
     <View style={styles.container}>
       <Text>Welcome to Home Screen!</Text>
     </View>
